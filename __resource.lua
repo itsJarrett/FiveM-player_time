84ce405cc16224ededb4c814bc83fadf8e76631b
Jarrett B <jarrettboice@gmail.com>
2018-09-07 18:52:30 -0700

    Initial

---
+++ __resource.lua
@@ -0,0 +1,3 @@
+resource_manifest_version '679-996150c95a1d251a5c0c7841ab2f0276878334f7'
+description 'Player Time Tracker'
+server_script 'sv_playertime.lua'
---
+++ sv_playertime.lua
@@ -0,0 +1,28 @@
+local playerTimes = {}
+local waitTime = 60000 -- We wait this amount in milliseconds until we check their time, (some people like to drop their connection A LOT).
+
+Citizen.CreateThread(function()
+  AddEventHandler('playerConnecting', function(playerName)
+    local _source = source
+    local steamID = GetPlayerIdentifier(source ,0)
+    steamID = steamID:gsub('steam:', '')
+    local joinTime = os.time()
+    Citizen.Wait(waitTime)
+    table.insert(playerTimes, {steamID, joinTime})
+  end)
+end)
+
+Citizen.CreateThread(function()
+  AddEventHandler('playerDropped', function(playerName)
+    local _source = source
+    local steamID = GetPlayerIdentifier(source ,0)
+    steamID = steamID:gsub('steam:', '')
+    local dropTime = os.time()
+    for _, playerTime in pairs(playerTimes) do
+      if playerTime[1] == steamID then
+        local totalTime = dropTime - playerTime[2]
+        MySQL.Sync.execute("UPDATE player SET playtime=@totalTime WHERE steamhex=@steamID", {['@steamID'] = steamID, ['@totalTime'] = totalTime})
+      end
+    end
+  end)
+end)
